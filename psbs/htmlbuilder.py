1545528d23624c3e51269dbfa4cdd9e74c683bfc
izzy kestrel <36773942+iznaut@users.noreply.github.com>
2024-11-07 12:03:02 -0600

    again

--- psbs/htmlbuilder.py
+++ psbs/htmlbuilder.py
@@ -40,7 +40,7 @@ def build_html(engine, source):
         PSBSError: If any errors occur during the building process.
     """
     # Fetch the standalone inlined HTML template
-    f = open("./standalone_inlined.txt", "r")
+    f = open("../standalone_inlined.txt", "r")
     # standalone_url = url_join(engine, "standalone_inlined.txt")
     # response = get(standalone_url, timeout=5)
     # if response.status_code != 200:
